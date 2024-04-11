624406765b5f802674e78a908f94899e4046610f
YerosenHunduma <yerosenhunduma@gmail.com>
2024-04-11 18:16:10 +0300

    specify the types of package

--- controllers/payment.js
+++ controllers/payment.js
@@ -12,6 +12,7 @@ const PaymentService = async (req, res) => {
 
   const { Fname, Lname, email, amount } = req.body;
   console.log(req.body);
+  console.log(amount);
   if (amount !== 100 || 500 || 1000) {
     return res.status(400).json({ success: false, message: "Invalid amount" });
   }
